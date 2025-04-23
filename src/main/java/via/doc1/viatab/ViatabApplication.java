498f1ddacd2cd69b66b888e2546dee9419c4ff84
Sakaadar <79919443+Sakaadar@users.noreply.github.com>
2025-04-23 16:21:35 +0200

    Initial commit

---
+++ src/main/java/via/doc1/viatab/ViatabApplication.java
@@ -0,0 +1,13 @@
+package via.doc1.viatab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViatabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ViatabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/via/doc1/viatab/ViatabApplicationTests.java
@@ -0,0 +1,13 @@
+package via.doc1.viatab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ViatabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
